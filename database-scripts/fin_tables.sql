84221d08de2308a3336bdeac26f015cc75a9f9b7
gcrespo <gcrespo1705@gmail.com>
2026-02-17 01:58:26 -0400

    created scripts for create tables in database

---
+++ database-scripts/fin_tables.sql
@@ -0,0 +1,32 @@
+CREATE TABLE LOCATIONS(
+  id_location SERIAL PRIMARY KEY,
+  name VARCHAR(150) UNIQUE NOT NULL,
+  type VARCHAR(15) NOT NULL,
+  id_parent_location NUMERIC,
+  CONSTRAINT fk_id_parent_location FOREIGN KEY (id_parent_location) REFERENCES LOCATIONS(id_location),
+  CONSTRAINT parent_check CHECK((type = 'COUNTRY' AND id_parent_location IS NULL)
+                                OR (type = 'CITY' AND id_parent_location IS NOT NULL)),
+  CONSTRAINT type_values_check CHECK(type = 'COUNTRY' OR type = 'CITY')
+);
+
+CREATE TABLE COMPANIES(
+  id_company SERIAL PRIMARY KEY,
+  symbol VARCHAR(4) UNIQUE NOT NULL,
+  name VARCHAR(100) UNIQUE NOT NULL,
+  id_location NUMERIC NOT NULL,
+  industry VARCHAR(150),
+  secotr VARCHAR(150),
+  CONSTRAINT fk_id_location FOREIGN KEY(id_location) REFERENCES LOCATIONS(id_location)
+);
+
+CREATE TABLE STOCK_HISTORY(
+  id_stock SERIAL PRIMARY KEY,
+  date DATE NOT NULL,
+  open NUMERIC(20, 8) NOT NULL,
+  high NUMERIC(20, 8) NOT NULL,
+  low NUMERIC(20, 8) NOT NULL,
+  close NUMERIC(20, 8) NOT NULL,
+  volume NUMERIC(20, 8) NOT NULL,
+  id_company NUMERIC NOT NULL,
+  CONSTRAINT fk_id_company FOREIGN KEY(id_company) REFERENCES COMPANIES(id_company)
+);
