64dbe7df8869ad873c0e54c42385a8f84cf35ba7
gcrespo <gcrespo1705@gmail.com>
2026-02-18 02:42:01 -0400

    created unit tests for extraction and cleaning functions

---
+++ tests/test_ingestion.py
@@ -0,0 +1,35 @@
+from data_pipeline.ingestion import get_companies_from_csv, fetch_companies_data, clean_companies_data
+import pytest
+import yfinance as yf
+
+def test_get_companies_from_csv():
+    file_name = '../data_pipeline/companies.csv'
+    companies_list = get_companies_from_csv(file_name)
+    companies_size = len(companies_list)
+    assert isinstance(companies_list, list) , "should be a list"
+    assert companies_size > 0 ,"should not be empty"
+
+def test_fetch_companies_data(monkeypatch):
+    symbol_list = ['MSFT', 'AAPL']
+    class MockTicker:
+        def __init__(self, symbol):
+            self.info = {'symbol':symbol, 'displayName':'MOCK_MSFT', 'sector':'Technology'}
+
+    monkeypatch.setattr(yf, 'Ticker', MockTicker)
+    companies_dict_list = fetch_companies_data(symbol_list)
+    first_element = companies_dict_list[0]
+
+    assert isinstance(companies_dict_list, list), "should be a list"
+    assert len(companies_dict_list) == 2, "should be a list of 2 elements"
+    assert first_element['symbol'] == 'MSFT', "should be MSFT"
+
+def test_clean_companies_data():
+    dirty_data = [{'symbol':'MSFT', 'displayName':'Microsoft', 'sector':'Technology', 'toDelete':'delete'}]
+
+    result = clean_companies_data(dirty_data)
+    element = result[0]
+
+    assert 'toDelete' not in element, "should not have this key"
+    assert element['symbol'] == 'MSFT', "should have symbol:MSFT"
+    
+
