62c04309a1cb1f93776ae321d017a5791cf99c61
gcrespo <gcrespo1705@gmail.com>
2026-02-20 02:19:19 -0400

    added functions to get symbols from db, fetch and clean stock history data

--- data_pipeline/ingestion.py
+++ data_pipeline/ingestion.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import csv
-from . import database_utils as db
+from data_pipeline import database_utils as db
+import json
 
 def get_companies_from_csv(file_name):
     companies = []
@@ -26,9 +27,31 @@ def load_companies_data(companies_list):
     database_utils = db.DatabaseUtilites()
     database_utils.bulk_insert_companies(companies_list)
 
+def get_symbols_from_db():
+    database_utils = db.DatabaseUtilites()
+    return database_utils.get_symbols()
+
+def fetch_stock_history(companies_symbol):
+    fetching_period = "6mo"
+    companies = ' '.join(companies_symbol)
+    history = yf.Tickers(companies).history(period=fetching_period)
+    return history
+
+def clean_stock_history_data(stock_history):
+    stock_history = stock_history.drop(columns = ['Stock Splits', 'Dividends'])
+    stock_history = stock_history.stack(level=1)
+    stock_history = stock_history.reset_index()
+    stock_history['Date'] = stock_history['Date'].dt.strftime('%Y-%m-%d')
+    stock_history = stock_history.rename(columns = {"Ticker":"Symbol"})
+    history_dict = json.loads(stock_history.to_json(orient = 'records'))
+    return history_dict
+    
+
+        
 
 if __name__ == "__main__":
-    file_name = './companies.csv'
-    required_companies = get_companies_from_csv(file_name)
-    companies_data = fetch_companies_data(required_companies)
-    load_companies_data(clean_companies_data(companies_data))
+    # file_name = './companies.csv'
+    # required_companies = get_companies_from_csv(file_name)
+    # companies_data = fetch_companies_data(required_companies)
+    # load_companies_data(clean_companies_data(companies_data))
+    print(clean_stock_history_data(fetch_stock_history(get_symbols_from_db())))
